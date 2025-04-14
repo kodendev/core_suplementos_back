027ca01b1f70aabd1c944a1704bc8122a82f890d
gonzafreddi <freddigonzalo17@gmail.com>
2025-04-14 15:28:14 -0300

    add config conection db

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,28 @@ import { OrdersModule } from './orders/orders.module';
 import { OrderItemsModule } from './order_items/order_items.module';
 import { CartItemsModule } from './cart_items/cart_items.module';
 import { ShipmentsModule } from './shipments/shipments.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UsersModule, ProductsModule, CategoriesModule, OrdersModule, OrderItemsModule, CartItemsModule, ShipmentsModule],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    CategoriesModule,
+    OrdersModule,
+    OrderItemsModule,
+    CartItemsModule,
+    ShipmentsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Path to your entities
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
