4c20b0145e372d04dae93134c6c4c2d23b7c4fdd
gonzafreddi <freddigonzalo17@gmail.com>
2025-04-14 12:35:48 -0300

    add main modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { OrdersModule } from './orders/orders.module';
+import { OrderItemsModule } from './order_items/order_items.module';
+import { CartItemsModule } from './cart_items/cart_items.module';
+import { ShipmentsModule } from './shipments/shipments.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule, CategoriesModule, OrdersModule, OrderItemsModule, CartItemsModule, ShipmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
